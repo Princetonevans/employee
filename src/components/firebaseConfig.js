8d40f10067a2557a33ea01a39b3dd035dc1d97ec
Princeton Evans <princetonevans@Princetons-MBP.hsd1.tx.comcast.net>
2018-02-19 12:23:46 -0600

    imported firebase for web app

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCeDFk4cv7GRpHTT-YnwbvsKmYaDkQXjWY",
+    authDomain: "employee-84074.firebaseapp.com",
+    databaseURL: "https://employee-84074.firebaseio.com",
+    projectId: "employee-84074",
+    storageBucket: "employee-84074.appspot.com",
+    messagingSenderId: "1091417024826"
+}
