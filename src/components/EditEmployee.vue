0bcd7e78097db48198efd785d1f4871adb8b8560
Princeton Evans <princetonevans@Princetons-MBP.hsd1.tx.comcast.net>
2018-02-20 15:52:20 -0600

    added navigation guard

--- src/components/EditEmployee.vue
+++ src/components/EditEmployee.vue
@@ -5,11 +5,32 @@
 </template>
 
 <script>
+import db from './firebaseInit'
 export default {
 name: 'edit-employee',
 data (){
   return {
+    employee_id: null,
+    name: null,
+    dept: null,
+    position: null
   }
- }
+},
+beforeRouteEnter (to, from, next) {
+  db.collection('employees').where('employee_id', '==', to.params.employee_id).get().then(querySnapshot => {
+    querySnapshot.foreach(doc => {
+      next(vm => {
+        vm.employee_id = doc.data().employee_id
+        vm.name = doc.data().name
+        vm.dept = doc.data().dept
+        vm.position = doc.data().position
+      })
+    })
+  })
+},
+watch: {
+  '$route': 'fetchData'
+},
+
 }
 </script>
