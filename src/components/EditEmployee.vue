a93f55e08a77404b61bcce57cbeb2b886867a6c8
Princeton Evans <princetonevans@yahoo.com>
2018-02-20 17:39:47 -0600

    added form to edit page

--- src/components/EditEmployee.vue
+++ src/components/EditEmployee.vue
@@ -1,8 +1,32 @@
 <template>
  <div id="edit-employee">
-   <ul class="collection with-header">
-     <li class="collection-header"><h4>{{name}}</h4></li>
-     </ul>
+ <h3>Edit Employee</h3>
+ <div class="row">
+ <form @submit.prevent="updateEmployee" class="col s12">
+   <div class="row">
+     <div class="input-field col s12">
+       <input type="text" v-model="employee_id" required>
+     </div>
+   </div>
+   <div class="row">
+     <div class="input-field col s12">
+       <input type="text" v-model="name" required>
+     </div>
+   </div>
+   <div class="row">
+     <div class="input-field col s12">
+       <input type="text" v-model="dept" required>
+     </div>
+   </div>
+   <div class="row">
+     <div class="input-field col s12">
+       <input type="text" v-model="position" required>
+     </div>
+   </div>
+   <button type="submit" class="btn">Submit</button>
+   <router-link to="/" class="btn grey">Cancel</router-link>
+ </form>
+</div>
 
 </div>
 </template>
