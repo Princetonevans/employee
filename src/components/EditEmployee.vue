d87fe2e43a34accadc15571331e580bc5cd8e57c
Princeton Evans <princetonevans@yahoo.com>
2018-02-20 16:49:15 -0600

    fetch data showing on page

--- src/components/EditEmployee.vue
+++ src/components/EditEmployee.vue
@@ -18,33 +18,6 @@ data (){
     dept: null,
     position: null
   }
-},
-beforeRouteEnter (to, from, next) {
-  db.collection('employees').where('employee_id', '==', to.params.employee_id).get().then(querySnapshot => {
-    querySnapshot.foreach(doc => {
-      next(vm => {
-        vm.employee_id = doc.data().employee_id
-        vm.name = doc.data().name
-        vm.dept = doc.data().dept
-        vm.position = doc.data().position
-      })
-    })
-  })
-},
-watch: {
-  '$route': 'fetchData'
-},
-methods: {
-  fetchData () {
-    db.collection('employees').where('employee_id', '==', this.$route.params.emplyee_id).get().then(querySnapshot => {
-      querySnapshot.forEach(doc => {
-        this.employee_id = doc.data().employee_id
-        this.name = doc.data().name
-        this.dept = doc.data().dept
-        this.position = doc.data().position
-      })
-    })
-  }
  }
 }
 </script>
